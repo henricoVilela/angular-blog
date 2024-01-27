bf4f26e648646c4aee1c85cc5c599b8ace9be8a0
Henrico Vilela <henricoVilela@gmail.com>
2024-01-27 12:44:17 -0400

    Tornar pagina de conteúdo responsiva

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -10,21 +10,28 @@ export const dataFake = [
     "id":"2",
     "principal": false,
     "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
+    "description": "Duis a lobortis dui, sed rutrum erat. Cras sit amet mi at orci pharetra efficitur quis eu turpis. Morbi libero dolor, dignissim euismod lacinia nec, blandit quis nisi. Quisque molestie tincidunt erat, id iaculis sem mattis in. Ut tempus laoreet nibh, id vestibulum nunc molestie non. Fusce consequat mauris ex. Nullam vestibulum, risus quis consectetur lobortis, nibh dui sodales justo, non scelerisque augue turpis at nulla. Duis vel bibendum magna. Donec metus elit, accumsan vitae pretium sit amet, tempus a lacus. Nullam maximus, tortor ut porta porta, augue ligula fringilla nunc, id pretium dolor felis eget nisl. Aenean imperdiet risus quis rutrum vestibulum. Aenean tincidunt blandit ex, a blandit augue",
     "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
   },
   {
     "id":"3",
     "principal": false,
     "title": "Novo filme do pantera negra lançado em breve",
-    "description": "bla blabla",
+    "description": "Duis a lobortis dui, sed rutrum erat. Cras sit amet mi at orci pharetra efficitur quis eu turpis. Morbi libero dolor, dignissim euismod lacinia nec, blandit quis nisi. Quisque molestie tincidunt erat, id iaculis sem mattis in. Ut tempus laoreet nibh, id vestibulum nunc molestie non. Fusce consequat mauris ex. Nullam vestibulum, risus quis consectetur lobortis, nibh dui sodales justo, non scelerisque augue turpis at nulla. Duis vel bibendum magna. Donec metus elit, accumsan vitae pretium sit amet, tempus a lacus. Nullam maximus, tortor ut porta porta, augue ligula fringilla nunc, id pretium dolor felis eget nisl. Aenean imperdiet risus quis rutrum vestibulum. Aenean tincidunt blandit ex, a blandit augue",
     "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
   {
     "id":"4",
     "principal": false,
     "title": "Novo filme dos X-men está em pós produção, afirma diretor",
-    "description": "bla blabla",
+    "description": "Duis a lobortis dui, sed rutrum erat. Cras sit amet mi at orci pharetra efficitur quis eu turpis. Morbi libero dolor, dignissim euismod lacinia nec, blandit quis nisi. Quisque molestie tincidunt erat, id iaculis sem mattis in. Ut tempus laoreet nibh, id vestibulum nunc molestie non. Fusce consequat mauris ex. Nullam vestibulum, risus quis consectetur lobortis, nibh dui sodales justo, non scelerisque augue turpis at nulla. Duis vel bibendum magna. Donec metus elit, accumsan vitae pretium sit amet, tempus a lacus. Nullam maximus, tortor ut porta porta, augue ligula fringilla nunc, id pretium dolor felis eget nisl. Aenean imperdiet risus quis rutrum vestibulum. Aenean tincidunt blandit ex, a blandit augue",
     "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
   },
+  {
+    "id":"5",
+    "principal": false,
+    "title": "Vingadores apareceram antes do MCU, mas completamente diferentes de hoje",
+    "description": "Duis a lobortis dui, sed rutrum erat. Cras sit amet mi at orci pharetra efficitur quis eu turpis. Morbi libero dolor, dignissim euismod lacinia nec, blandit quis nisi. Quisque molestie tincidunt erat, id iaculis sem mattis in. Ut tempus laoreet nibh, id vestibulum nunc molestie non. Fusce consequat mauris ex. Nullam vestibulum, risus quis consectetur lobortis, nibh dui sodales justo, non scelerisque augue turpis at nulla. Duis vel bibendum magna. Donec metus elit, accumsan vitae pretium sit amet, tempus a lacus. Nullam maximus, tortor ut porta porta, augue ligula fringilla nunc, id pretium dolor felis eget nisl. Aenean imperdiet risus quis rutrum vestibulum. Aenean tincidunt blandit ex, a blandit augue",
+    "photoCover":"https://img.olhardigital.com.br/wp-content/uploads/2019/04/20190417112032.jpg"
+  }
 ]
