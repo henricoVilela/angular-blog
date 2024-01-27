b7001f6c6643c5f46a1dc04b837314791227d202
Henrico Vilela <henricoVilela@gmail.com>
2024-01-27 12:09:50 -0400

    Melhorar listagem das noticias
    - Fazer uso do ngFor par criar os smallcards
    - Adicionar icones das redes sociais

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public noticiasSecundarias: any = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.noticiasSecundarias = dataFake.filter(data => !data.principal);
   }
 
 }
